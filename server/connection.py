206c872f46f016c9a7d2107fac45cfe85799a63d
mcibula <mirkocibula@gmail.com>
2023-02-08 17:52:59 +0100

    Socket server and connection docs added and minor fixes

--- server/connection.py
+++ server/connection.py
@@ -5,23 +5,48 @@ from typing import Tuple, List
 
 class Connection:
     def __init__(self, host: str = '127.0.0.1', port: int = 65432) -> None:
+        """
+        Class managing a socket connection
+
+        :param host: IP address of the server
+        :param port: Port of the server
+        """
+
         self.host = host
         self.port = port
 
+        # Connect to the server
         self._connect()
 
     def __del__(self) -> None:
+        """
+        Shutdown and close the connection on a destruction
+        """
+
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
 
     def _connect(self) -> None:
+        """
+        Create a new connection with the server
+        """
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
 
     def _send(self, data: bytes) -> None:
+        """
+        Send data
+
+        :param data: Data to send
+        """
+
         try:
+            # Send all data through the connection
             self.socket.sendall(data)
         except BrokenPipeError:
+            # If the connection is not alive anymore
+            # Create a new one and repeat
             self._connect()
             self.socket.sendall(data)
 
@@ -56,14 +81,13 @@ class Connection:
 
     def get_joint_config(self) -> List[float]:
         self._send(b'get_joint_config')
-        data = b''
+        q = []
 
         try:
             data = self.socket.recv(1024)
+            q = pickle.loads(data)
         except EOFError:
             pass
-
-        q = pickle.loads(data)
 
         print(q)
 
--- server/server.py
+++ server/server.py
@@ -7,19 +7,33 @@ from typing import Tuple
 
 class ThreadedServer:
     def __init__(self, host: str = '127.0.0.1', port: int = 65432) -> None:
+        """
+        Multithreaded TCP socket server serving multiple clients
+
+        :param host: IP of the server
+        :param port: Serving port
+        """
+
+        # Initialize socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((host, port))
 
     def listen(self) -> None:
+        """
+        Create a new connection
+        """
+
         self.socket.listen()
 
         while True:
+            # Accept incoming connection and set an inactivity timeout to 30 seconds
             conn, addr = self.socket.accept()
             conn.settimeout(30)
 
             print(f'Connected to {addr}')
 
+            # Start a new listening thread
             threading.Thread(
                 target=self.receive,
                 args=(conn, addr)
@@ -27,21 +41,37 @@ class ThreadedServer:
 
     @staticmethod
     def receive(conn: socket.socket, addr: Tuple[str, int]) -> bool:
+        """
+        Receive and/or send data
+
+        :param conn: Open connection socket
+        :param addr: Tuple of an IP address and port of a client
+
+        :return: False, if the connection has timed out
+        """
+
         while True:
             try:
+                # Receive 1024 B of data
                 data = conn.recv(1024)
 
+                # If there is some data
                 if data:
                     print(data.decode())
 
+                # If `get_joint_config` request is received
                 if data.decode() == 'get_joint_config':
+                    # Send back new data
                     deg = math.pi / 180
                     conn.sendall(pickle.dumps([0 * deg, 0 * deg, 0 * deg, 45 * deg, 80 * deg, 0 * deg]))
 
             except socket.timeout:
+                # In case of time out
+                # Close the connection
                 conn.close()
                 print(f'Connection to {addr} has been closed')
 
+                # Terminate this thread
                 return False
 
 
